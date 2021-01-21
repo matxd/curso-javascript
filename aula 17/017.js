dd73126f569a6e263efa31185bafc377e5538eba
matxd <matheusrcrv07@gmail.com>
2021-01-21 14:05:12 -0300

    update

---
+++ aula 17/017.js
@@ -0,0 +1,6 @@
+var amigo = {nome: "Matheus", peso: 68.7, idade: 18, engordar(p=0){
+    console.log("Engordou")
+    this.peso += p
+}}
+amigo.engordar(2)
+console.log(`O amigo se chama ${amigo.nome}, pesa ${amigo.peso} e tem ${amigo.idade} anos de idade.`)
